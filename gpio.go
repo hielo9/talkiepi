694c0b8a42b28cfd787e60822d706c205797a895
hielo9 <hielo9@users.noreply.github.com>
2020-10-09 22:44:07 -0700

    Update gpio.go
    clean up some behavior with beginning transmission

--- gpio.go
+++ gpio.go
@@ -23,9 +23,9 @@ func (b *Talkiepi) initGPIO() {
 
 	rpio.Close()
 	
-	upTime = 0
-	downTime = 0
-	click = 0
+	var upTime int = 0
+	var downTime int = 0
+	var click int = 0
 
 	// unfortunately the gpio watcher stuff doesnt work for me in this context, so we have to poll the button instead
 	b.Button = gpio.NewInput(ButtonPin)
@@ -34,20 +34,25 @@ func (b *Talkiepi) initGPIO() {
 			currentState, err := b.Button.Read()
 
 			if currentState != b.ButtonState && err == nil {
-				//b.ButtonState = currentState
+				b.ButtonState = currentState
 
 				if b.Stream != nil {
 					if b.ButtonState == 1 {
 						fmt.Printf("Button is released\n")
+						upTime = 0
 						if downTime<250 {
 							click = click+1
+							fmt.Printf("add a click %d\n", click)
 							if click==2 {  // this is a double click and we need to toggle the connection
 								if b.IsConnected == false {
+									fmt.Printf("click2 false\n")
 									b.Connect()
 								} else {
+									fmt.Printf("click2 false\n")
 									b.Client.Disconnect()
 								}
 						} else {
+							fmt.Printf("stop transmitting\n")
 							b.TransmitStop()
 						}
 					} else {
@@ -57,19 +62,26 @@ func (b *Talkiepi) initGPIO() {
 						if upTime>500 {  // this obviously wasn't a double click
 							click = 0
 							downTime = 0
+							fmt.Printf("over 500\n")
 						}
 					}
 				}
 
 			} else {
 				if currentState == 1 {
-					upTime = upTime+10
+					if upTime < 1000 {
+						upTime = upTime+10
+					}
 				} else {
-					downTime = downTime+10
-				}
-				if downTime>250 && b.Stream != nil && err==nil {  // all right, it's down long enough to assume this is transmission
-					b.TransmitStart()
-					click = 0
+					if downTime < 1000 {
+						downTime = downTime+10
+					}
+					if downTime>250 && b.Stream != nil && err==nil {  // all right, it's down long enough to assume this is transmission
+						fmt.Printf("start transmission\n")
+						b.TransmitStart()
+						click = 0
+						downTime = downTime + 1  // let's make sure if someone releases right at 250 ms, we don't keep coming in here (but I think I might have solved this by making sure currentState != 1
+					}
 				}
 			}
 				
